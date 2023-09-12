db50bc6eb70aa67849c4ef7d6389e7f8132023b8
Witanmi7 <ignacioferyoris@gmail.com>
2023-09-12 13:38:28 -0300

    segunda pre entrega

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,14 @@
+const task = new Promise(()=>{
+  
+})
+
+const ItemListContainer = (welcome) => {
+  console.log(task)
+  return (
+    <div>
+      {welcome.greeting}
+    </div>
+  )
+}
+
+export default ItemListContainer
